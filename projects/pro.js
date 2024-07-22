c08fa48c70e7bee2e73517cbd30efecd6242e13b
adityapowar88 <adityapowar8888@gmail.com>
2024-07-22 15:02:48 +0530

    Add new features to project section

--- projects/pro.js
+++ projects/pro.js
@@ -1,7 +1,7 @@
 document.getElementById('backButton').addEventListener('click', function() {
-    window.location.href = '../web.html'; 
+    window.location.href = '../index.html'; 
 });
 
 document.getElementById('backButton2').addEventListener('click', function() {
-    window.location.href = '../web.html'; 
+    window.location.href = '../index.html'; 
 });
